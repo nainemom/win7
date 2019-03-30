907398c9bbcd043046891ba9df587e2e3a01cfa3
Amir Momenian <nainemom@gmail.com>
2019-03-31 03:04:11 +0430

    create downloading component

---
+++ components/AppDownloading.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="downloading"> 
+    <div> Windows is starting </div>  
+    <div class="loading"><span v-for="i in count" :key="i">.</span></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      timer: null,
+      count: 1
+    }
+  },
+  created() {
+    this.timer = setInterval(() => {
+      this.count++
+      if (this.count === 100) {
+        this.count = 1
+      }
+    }, 700)
+  },
+  beforeDestroy() {
+    clearInterval(this.timer)
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.downloading {
+  background: #000;
+  color: #fff;
+  padding: 10%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 999;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  & .loading {
+    padding: 15px 0;
+  }
+}
+</style>
