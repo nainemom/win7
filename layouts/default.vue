4d50755bf3481031f9c3406b84e4ccc1fa451fd5
Amir Momenian <nainemom@gmail.com>
2019-03-31 01:09:32 +0430

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <nuxt />
+</template>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+html, body, #__nuxt, #__layout {
+  height: 100%;
+  min-height: 100%;
+  max-height: 100%;
+  overflow: hidden;
+  padding: 0;
+  margin: 0;
+  background-size: cover;
+  background-position: bottom;
+  user-select: none;
+}
+</style>
