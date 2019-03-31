12e0d239f5afa47824b3b1d8ba3f886a7114315f
Amir Momenian <nainemom@gmail.com>
2019-04-01 02:34:46 +0430

    create camera component

---
+++ components/AppCamera.vue
@@ -0,0 +1,27 @@
+<template>
+    <video class="camera" muted autoplay playsinline />
+</template>
+<script>
+export default {
+  mounted() {
+    navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+        facingMode: 'user'
+      }
+    }).then(stream => {
+      this.$el.srcObject = stream;
+    });
+  }
+}
+</script>
+
+<style>
+  .camera {
+    width: 100%;
+    height: 100%;
+    border: none;
+    resize: none;
+  }
+</style>
+
