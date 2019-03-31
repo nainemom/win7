8ab81e5d25d4c95129adb7d8ad24e60add68204c
Amir Momenian <nainemom@gmail.com>
2019-03-31 18:20:54 +0430

    fix background position

--- components/AppBackground.vue
+++ components/AppBackground.vue
@@ -10,6 +10,8 @@
     left: 0;
     width: 100%;
     height: 100%;
-    background-position: center;
+    background-position: bottom center;
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
   }
 </style>
