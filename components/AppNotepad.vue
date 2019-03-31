39b1b374d03baeda4610f3aa0f090c80c328ca6b
Amir Momenian <nainemom@gmail.com>
2019-04-01 02:35:02 +0430

    create notepad component

---
+++ components/AppNotepad.vue
@@ -0,0 +1,13 @@
+<template>
+    <textarea class="notepad" :value="$attrs.content || ''" />
+</template>
+<style>
+  .notepad {
+    width: 100%;
+    height: 100%;
+    border: none;
+    padding: 8px;
+    resize: none;
+  }
+</style>
+
