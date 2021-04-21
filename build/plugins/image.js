c8c9d7b9f191699d1083f88f7f2a8fc57d50caf0
Amir Momenian <nainemom@gmail.com>
2021-04-22 01:48:23 +0430

    image to dataUri

---
+++ build/plugins/image.js
@@ -0,0 +1,15 @@
+import datauri from 'datauri';
+const fileRegex = /\.(png|ico|jpg|svg)$/;
+
+
+export default () => ({
+  name: 'image',
+  transform(_src, id) {
+    if (fileRegex.test(id)) {
+      return datauri(id).then((data) => ({
+        map: null,
+        code: `export default '${data}';`,
+      }));
+    }
+  }
+});
