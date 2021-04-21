355b17f2bfcdf18d79bf3bfb123184d45d75efd9
Amir Momenian <nainemom@gmail.com>
2021-04-22 04:25:06 +0430

    add deploy command

---
+++ build/deploy.js
@@ -0,0 +1,12 @@
+const process = require('process');
+const ghpages = require('gh-pages');
+
+ghpages.publish('dist', (err) => {
+  if (err) {
+    console.error('Deploy Error!!!');
+    process.exit(1);
+  } else {
+    console.log('Deployed!!!');
+    process.exit(0);
+  }
+});
