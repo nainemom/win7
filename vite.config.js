6d32fb56f46cc94e5eec4738c3282b7d1ddac8d5
Seyyed Hassan Asheghi <asheghi.sh@gmail.com>
2021-08-15 23:52:35 +0430

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import datauri from './build/plugins/datauri';
-import files from './build/plugins/files';
 
 export default defineConfig({
   plugins: [
     vue(),
-    datauri(),
-    files(),
   ],
 });
