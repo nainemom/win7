0e0864fe229775b0c7a18a054d7e3d33cba15b3c
Seyyed Hassan Asheghi <asheghi.sh@gmail.com>
2021-08-16 13:55:20 +0430

    changed broserfs back-end to MountableFileSystem and IndexedDB

--- src/services/fs.js
+++ src/services/fs.js
@@ -4,127 +4,11 @@ import { getFileType } from './apps';
 
 import { extname, basename, dirname } from 'path-browserify';
 
-export function isFile(filePath) {
-  return !!extname(filePath);
-}
-
-export function isDirectory(filePath) {
-  return !extname(filePath);
-}
-
-const FS_BACKEND = 'InMemory';
-const FS_BACKEND_OPTIONS = {};
-
-export function reverseSlash(filePath) {
-  return filePath.replace(/\//g, '/');
-}
-
 let fs = {};
-
-function getFS() {
-  return new Promise((resolve, reject) => {
-    BrowserFS.configure({
-      fs: FS_BACKEND,
-      options: FS_BACKEND_OPTIONS,
-    }, function (e) {
-      if (e) {
-        reject(e);
-      }
-      const _fs = BrowserFS.BFSRequire('fs');
-      resolve(_fs);
-    });
-  });
-}
-
-function _writePrograms() {
-  //actual vue applications
-  fs.writeFileSync('/C:/Windows/System32/Explorer.exe', 'Explorer', { encoding: 'utf8' });
-  fs.writeFileSync('/C:/Windows/System32/Notepad.exe', 'Notepad', { encoding: 'utf8' });
-  fs.writeFileSync('/C:/Windows/System32/MediaPlayer.exe', 'MediaPlayer', { encoding: 'utf8' });
-  //fs.writeFileSync('/C:/Windows/System32/BluePage.exe', 'BluePage', { encoding: 'utf8' });
-  fs.writeFileSync('/C:/Program Files/Camera.exe', 'Camera', { encoding: 'utf8' });
-
-  //web apps
-  const methodDraw = {
-    'icon': 'https://editor.method.ac/images/favicon.svg',
-    'url': 'https://editor.method.ac/',
-    'width': 900,
-    'height': 700
-  };
-  const snap =   {
-    "icon": "https://passenger-pwa-cdn.snapp.ir/logos/square-minimal-144.png",
-    "url": "https://app.snapp.taxi/",
-    "width": 500,
-    "height": 800
-  };
-  const Viska = {
-    "icon": "https://viska.chat/logo-transparent.png",
-    "url": "https://viska.chat/",
-    "width": 500,
-    "height": 800
-  }
-
-  const win93 = {
-    "icon": "https://v1.windows93.net/favicon.ico",
-    "url": "https://v1.windows93.net/",
-    "width": 1000,
-    "height": 800
-  }
-
-  fs.writeFileSync('/C:/Program Files/Method Draw.wapp', JSON.stringify(methodDraw), { encoding: 'utf8' });
-  fs.writeFileSync('/C:/Program Files/Snap.wapp', JSON.stringify(snap), { encoding: 'utf8' });
-  fs.writeFileSync('/C:/Program Files/Viska.wapp', JSON.stringify(Viska), { encoding: 'utf8' });
-  fs.writeFileSync('/C:/Program Files/win93.wapp', JSON.stringify(win93), { encoding: 'utf8' });
-
-  //shortcuts
-  fs.writeFileSync('/C:/User/Desktop/My Computer.link', '/C:/Windows/System32/Explorer.exe', { encoding: 'utf8' });
-  fs.writeFileSync('/C:/User/Desktop/Notepad.link', '/C:/Windows/System32/Notepad.exe', { encoding: 'utf8' });
-  fs.writeFileSync('/C:/User/Desktop/Hayde - Saghi.link', '/C:/User/Music/Hayde - Saghi.mp3', { encoding: 'utf8' });
-  fs.writeFileSync('/C:/User/Desktop/Method Draw.link', '/C:/Program Files/Method Draw.wapp', { encoding: 'utf8' });
-  fs.writeFileSync('/C:/User/Desktop/Snap!.link', '/C:/Program Files/Snap.wapp', { encoding: 'utf8' });
-  fs.writeFileSync('/C:/User/Desktop/Viska.link', '/C:/Program Files/Viska.wapp', { encoding: 'utf8' });
-  fs.writeFileSync('/C:/User/Desktop/win93.link', '/C:/Program Files/win93.wapp', { encoding: 'utf8' });
-
-
-  fs.mkdirSync('/C:/User/Desktop/New Folder');
-  fs.writeFileSync('/C:/User/Desktop/New Folder/Text File.txt', 'hello world', { encoding: 'utf8' });
-}
-
-function _writeStartMenuItem() {
-  fs.writeFileSync('/C:/User/Start Menu/My Computer.link', '/C:/Windows/System32/Explorer.exe', { encoding: 'utf8' });
-  fs.writeFileSync('/C:/User/Start Menu/Desktop.link', '/C:/User/Desktop', { encoding: 'utf8' });
-  fs.writeFileSync('/C:/User/Start Menu/Documents.link', '/C:/User/Documents', { encoding: 'utf8' });
-  fs.writeFileSync('/C:/User/Start Menu/Music.link', '/C:/User/Music', { encoding: 'utf8' });
-  fs.writeFileSync('/C:/User/Start Menu/Pictures.link', '/C:/User/Pictures', { encoding: 'utf8' });
-}
-
-async function populateFS() {
-  fs.mkdirSync('/C:');
-  fs.mkdirSync('/D:');
-
-  fs.mkdirSync('/C:/User');
-  fs.mkdirSync('/C:/Program Files');
-  fs.mkdirSync('/C:/Windows');
-  fs.mkdirSync('/C:/Windows/System32');
-
-  fs.mkdirSync('/C:/User/Desktop');
-  fs.mkdirSync('/C:/User/Documents');
-  fs.mkdirSync('/C:/User/Music');
-  fs.mkdirSync('/C:/User/Pictures');
-  fs.mkdirSync('/C:/User/Start Menu');
-
-  _writePrograms();
-  _writeStartMenuItem();
-
-  fs.writeFileSync('/C:/User/Desktop/TextFile.txt', 'hello world', { encoding: 'utf8' });
-  fs.writeFileSync('/C:/User/Music/Hayde - Saghi.mp3', 'https://dl.dl2musica.com/singles/0003/Hayedeh%20-%20Saghi.mp3', { encoding: 'utf8' });
-}
 
 export async function initFS() {
   fs = await getFS();
-  if (FS_BACKEND === 'InMemory') {
-    await populateFS();
-  }
+  await populateFS();
 }
 
 export function createNewFile(fileObject) {
@@ -156,7 +40,7 @@ export function fileRef(path, type) {
 }
 
 export async function readDirectory(path) {
-  await new Promise(r => setTimeout(r, 500));
+  //await new Promise(r => setTimeout(r, 500));
   return new Promise((resolve, reject) => {
     fs.readdir(path, function (e, data) {
       if (e) {
@@ -170,7 +54,8 @@ export async function readDirectory(path) {
 
 export async function fetchTextFile(path) {
   //await new Promise(r => setTimeout(r,1500));
-  return fetchFile(path, { encoding: 'utf8' });
+  //return fetchFile(path, { encoding: 'utf8' });
+  return fetchFile(path,{encoding:'utf8'})
 }
 
 export async function fetchFile(path, options = {}) {
@@ -203,48 +88,17 @@ export async function deleteFile(filePath) {
   });
 }
 
-function isEmptyDirectory(filePath) {
-  const list = fs.readdirSync(filePath);
-  return list.length === 0;
-}
-
 export async function deleteDirectory(filePath) {
-  if (isEmptyDirectory(filePath)) {
-    fs.rmdirSync(filePath);
+  if (await isEmptyDirectory(filePath)) {
+    await fs.rmdir(filePath);
   } else {
     throw new Error('Folder is not empty!');
   }
 }
 
-function existsPath(filePath) {
-  return fs.existsSync(filePath);
-}
-
-function getNewFolderName(baseDir) {
-  const newName = (i) => `New Folder${i > 1 ? ` (${i})` : ''}`;
-  let i = 1;
-  let folderName;
-  do {
-    folderName = join(baseDir, newName(i));
-    i += 1;
-  } while (existsPath(folderName));
-  return newName(i - 1);
-}
-
-function getNewTextFileName(baseDir, fileName = 'Text File') {
-  const newName = (i) => `${fileName}${i > 1 ? ` (${i})` : ''}.txt`;
-  let i = 1;
-  let name;
-  do {
-    name = join(baseDir, newName(i));
-    i += 1;
-  } while (existsPath(name));
-  return newName(i - 1);
-}
-
 export async function createNewFolder(filePath, directoryName) {
   if (!directoryName) {
-    directoryName = getNewFolderName(filePath);
+    directoryName = await getNewFolderName(filePath);
   }
   return new Promise((resolve, reject) => {
     const newDirPath = join(filePath, directoryName);
@@ -258,8 +112,8 @@ export async function createNewFolder(filePath, directoryName) {
 }
 
 export async function createNewTextFile(filePath, fileName, content = '') {
-  if (!fileName || existsPath(join(filePath, fileName))) {
-    fileName = getNewTextFileName(filePath);
+  if (!fileName || await existsPath(join(filePath, fileName))) {
+    fileName = await getNewTextFileName(filePath);
   }
   return new Promise((resolve, reject) => {
     fs.writeFile(join(filePath, fileName), content, { encoding: 'utf8' }, function (e) {
@@ -291,11 +145,11 @@ export async function writeTextFile(filePath, content) {
 }
 
 export async function renamePath(filePath, newName) {
-  fs.renameSync(filePath, join(dirname(filePath), newName));
+  await fs.rename(filePath, join(dirname(filePath), newName));
 }
 
 export async function moveFile(filePath, directory) {
-  fs.renameSync(filePath, directory);
+  await fs.rename(filePath, directory);
 }
 
 export async function copyFile(filePath, directory) {
@@ -304,10 +158,166 @@ export async function copyFile(filePath, directory) {
   let targetFile;
   while (true) {
     targetFile = join(directory, fileName);
-    if (!fs.existsSync(targetFile)) {
+    let exists = await fs.exists(targetFile);
+    if (!exists) {
       break;
     }
     fileName = 'copy of ' + fileName;
   }
-  fs.writeFileSync(targetFile, fileContent);
+  await fs.writeFile(targetFile, fileContent);
+}
+
+export function isFile(filePath) {
+  return !!extname(filePath);
+}
+
+export function isDirectory(filePath) {
+  return !extname(filePath);
+}
+
+export function reverseSlash(filePath) {
+  return filePath.replace(/\//g, '/');
+}
+
+function getFS() {
+  return new Promise((resolve, reject) => {
+    BrowserFS.configure({
+      fs: 'MountableFileSystem',
+      options: {
+        '/C:': {
+          fs: 'IndexedDB',
+          options: {},
+        },
+        '/D:': {
+          fs: 'IndexedDB',
+          options: {}
+        },
+      },
+    }, function (e) {
+      if (e) {
+        console.error(e);
+        reject(e);
+      }
+      const _fs = BrowserFS.BFSRequire('fs');
+      resolve(_fs);
+    });
+  });
+}
+
+async function existsPath(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.exists(filePath,exists =>{
+      resolve(exists);
+    });
+  })
+}
+
+async function getNewFolderName(baseDir) {
+  const newName = (i) => `New Folder${i > 1 ? ` (${i})` : ''}`;
+  let i = 1;
+  let folderName;
+  do {
+    folderName = join(baseDir, newName(i));
+    i += 1;
+  } while (await existsPath(folderName));
+  return newName(i - 1);
+}
+
+async function getNewTextFileName(baseDir, fileName = 'Text File') {
+  const newName = (i) => `${fileName}${i > 1 ? ` (${i})` : ''}.txt`;
+  let i = 1;
+  let name;
+  do {
+    name = join(baseDir, newName(i));
+    i += 1;
+  } while (await existsPath(name));
+  return newName(i - 1);
+}
+
+async function isEmptyDirectory(filePath) {
+  const list = await fs.readdir(filePath);
+  return list.length === 0;
+}
+
+async function _writePrograms() {
+  //actual vue applications
+  await writeTextFile('/C:/Windows/System32/Explorer.exe', 'Explorer',);
+  await writeTextFile('/C:/Windows/System32/Notepad.exe', 'Notepad',);
+  await writeTextFile('/C:/Windows/System32/MediaPlayer.exe', 'MediaPlayer',);
+  //fs.writeFileSync('/C:/Windows/System32/BluePage.exe', 'BluePage', { encoding: 'utf8' });
+  await writeTextFile('/C:/Program Files/Camera.exe', 'Camera',);
+
+  //web apps
+  const methodDraw = {
+    'icon': 'https://editor.method.ac/images/favicon.svg',
+    'url': 'https://editor.method.ac/',
+    'width': 900,
+    'height': 700
+  };
+  const snap = {
+    'icon': 'https://passenger-pwa-cdn.snapp.ir/logos/square-minimal-144.png',
+    'url': 'https://app.snapp.taxi/',
+    'width': 500,
+    'height': 800
+  };
+  const Viska = {
+    'icon': 'https://viska.chat/logo-transparent.png',
+    'url': 'https://viska.chat/',
+    'width': 500,
+    'height': 800
+  };
+
+  const win93 = {
+    'icon': 'https://v1.windows93.net/favicon.ico',
+    'url': 'https://v1.windows93.net/',
+    'width': 1000,
+    'height': 800
+  };
+
+  await writeTextFile('/C:/Program Files/Method Draw.wapp', JSON.stringify(methodDraw),);
+  await writeTextFile('/C:/Program Files/Snap.wapp', JSON.stringify(snap),);
+  await writeTextFile('/C:/Program Files/Viska.wapp', JSON.stringify(Viska),);
+  await writeTextFile('/C:/Program Files/win93.wapp', JSON.stringify(win93),);
+
+  //shortcuts
+  await writeTextFile('/C:/User/Desktop/My Computer.link', '/C:/Windows/System32/Explorer.exe',);
+  await writeTextFile('/C:/User/Desktop/Notepad.link', '/C:/Windows/System32/Notepad.exe',);
+  await writeTextFile('/C:/User/Desktop/Hayde - Saghi.link', '/C:/User/Music/Hayde - Saghi.mp3',);
+  await writeTextFile('/C:/User/Desktop/Method Draw.link', '/C:/Program Files/Method Draw.wapp',);
+  await writeTextFile('/C:/User/Desktop/Snap!.link', '/C:/Program Files/Snap.wapp',);
+  await writeTextFile('/C:/User/Desktop/Viska.link', '/C:/Program Files/Viska.wapp',);
+  await writeTextFile('/C:/User/Desktop/win93.link', '/C:/Program Files/win93.wapp',);
+
+  await fs.mkdir('/C:/User/Desktop/New Folder');
+  await writeTextFile('/C:/User/Desktop/New Folder/Text File.txt', 'hello world',);
+}
+
+async function _writeStartMenuItem() {
+  await fs.writeFile('/C:/User/Start Menu/My Computer.link', '/C:/Windows/System32/Explorer.exe', { encoding: 'utf8' });
+  await fs.writeFile('/C:/User/Start Menu/Desktop.link', '/C:/User/Desktop', { encoding: 'utf8' });
+  await fs.writeFile('/C:/User/Start Menu/Documents.link', '/C:/User/Documents', { encoding: 'utf8' });
+  await fs.writeFile('/C:/User/Start Menu/Music.link', '/C:/User/Music', { encoding: 'utf8' });
+  await fs.writeFile('/C:/User/Start Menu/Pictures.link', '/C:/User/Pictures', { encoding: 'utf8' });
+}
+
+async function populateFS() {
+  await fs.mkdir('/C:');
+  await fs.mkdir('/D:');
+
+  await fs.mkdir('/C:/User');
+  await fs.mkdir('/C:/Program Files');
+  await fs.mkdir('/C:/Windows');
+  await fs.mkdir('/C:/Windows/System32');
+
+  await fs.mkdir('/C:/User/Desktop');
+  await fs.mkdir('/C:/User/Documents');
+  await fs.mkdir('/C:/User/Music');
+  await fs.mkdir('/C:/User/Pictures');
+  await fs.mkdir('/C:/User/Start Menu');
+
+  await _writePrograms();
+  await _writeStartMenuItem();
+
+  await fs.writeFile('/C:/User/Desktop/TextFile.txt', 'hello world', { encoding: 'utf8' });
+  await fs.writeFile('/C:/User/Music/Hayde - Saghi.mp3', 'https://dl.dl2musica.com/singles/0003/Hayedeh%20-%20Saghi.mp3', { encoding: 'utf8' });
 }
