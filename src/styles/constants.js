21b1bb155e178e4737a95e686ecdb087d86190d8
Amir Momenian <nainemom@gmail.com>
2021-08-16 12:57:13 +0430

    remove old wallpapers key and values

--- src/styles/constants.js
+++ src/styles/constants.js
@@ -2,19 +2,3 @@ export const panelSize = '46px';
 
 export const contextMenuWidth = '180px';
 export const contextMenuItemHeight = '30px';
-
-export const wallpapers = [
-  import('../../files/C:/Windows/wallpapers/1.jpg'),
-  import('../../files/C:/Windows/wallpapers/2.jpeg'),
-  import('../../files/C:/Windows/wallpapers/3.jpeg'),
-  import('../../files/C:/Windows/wallpapers/4.jpeg'),
-  import('../../files/C:/Windows/wallpapers/5.jpeg'),
-  import('../../files/C:/Windows/wallpapers/6.jpeg'),
-  import('../../files/C:/Windows/wallpapers/7.jpeg'),
-  import('../../files/C:/Windows/wallpapers/8.jpeg'),
-  import('../../files/C:/Windows/wallpapers/9.jpeg'),
-  import('../../files/C:/Windows/wallpapers/10.jpeg'),
-  import('../../files/C:/Windows/wallpapers/11.jpeg'),
-  import('../../files/C:/Windows/wallpapers/12.jpeg'),
-  import('../../files/C:/Windows/wallpapers/13.jpeg'),
-];
