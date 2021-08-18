dd067ee56948c118ae5bec4a36056498f6261bba
Amir Momenian <nainemom@gmail.com>
2021-08-18 16:04:10 +0430

    add 'resolve @' and replace plugin

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import datauri from './build/plugins/datauri';
+import path from 'path';
+import replace from '@rollup/plugin-replace';
 import files from './build/plugins/files';
 
 export default defineConfig({
   plugins: [
     vue(),
-    datauri(),
     files(),
+    replace({
+      preventAssignment: true,
+      values: {
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      },
+    }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '/src'),
+    },
+  },
 });
